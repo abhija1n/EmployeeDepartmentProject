1e8b04f6152e9dae5d340bf7fd9453e5ea496c5e
abhinavjain <abhinavj142@gmail.com>
2024-12-08 20:19:24 +0530

    remove spaces

--- src/main/java/com/assesment/spring/EmployeDepartmentService/EmployeDepartmentServiceApplication.java
+++ src/main/java/com/assesment/spring/EmployeDepartmentService/EmployeDepartmentServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeDepartmentServiceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(EmployeDepartmentServiceApplication.class, args);
 	}
 

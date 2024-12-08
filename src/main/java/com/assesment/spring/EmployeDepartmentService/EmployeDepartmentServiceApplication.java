6d57ca021ea2e2b2877c114aaea33de304329a23
abhinavjain <abhinavj142@gmail.com>
2024-12-08 21:28:40 +0530

    add one spaces

--- src/main/java/com/assesment/spring/EmployeDepartmentService/EmployeDepartmentServiceApplication.java
+++ src/main/java/com/assesment/spring/EmployeDepartmentService/EmployeDepartmentServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeDepartmentServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmployeDepartmentServiceApplication.class, args);
 	}
 

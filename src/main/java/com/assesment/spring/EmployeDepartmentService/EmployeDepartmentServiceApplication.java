d61f4f965792ecc480a6d18a6082eeabee81a37a
abhinavjain <abhinavj142@gmail.com>
2024-12-08 20:05:07 +0530

    first commit

---
+++ src/main/java/com/assesment/spring/EmployeDepartmentService/EmployeDepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assesment.spring.EmployeDepartmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeDepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeDepartmentServiceApplication.class, args);
+	}
+
+}
